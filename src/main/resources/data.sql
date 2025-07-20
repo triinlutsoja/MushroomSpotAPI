9fecd9f54c3eaab030d32204d490d9eb29d7df31
Triin Lutsoja <161822295+triinlutsoja@users.noreply.github.com>
2025-07-20 14:23:36 +0300

    Fix sample data
    - Made sure that sample spots are located in Estonia

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO mushroom_spot (coordinates, description)
 VALUES
-  (ST_SetSRID(ST_MakePoint(26.745, 57.434), 4326), 'Kukeseened'),
-  (ST_SetSRID(ST_MakePoint(24.754, 57.433), 4326), 'Puravikud'),
-  (ST_SetSRID(ST_MakePoint(24.742, 59.472), 4326), 'Männiriisikad'),
-  (ST_SetSRID(ST_MakePoint(24.735, 59.437), 4326), 'Kärbseseened'),
+  (ST_SetSRID(ST_MakePoint(23.961, 59.004), 4326), 'Kukeseened'),
+  (ST_SetSRID(ST_MakePoint(26.774, 59.139), 4326), 'Puravikud'),
+  (ST_SetSRID(ST_MakePoint(25.247, 58.981), 4326), 'Männiriisikad'),
+  (ST_SetSRID(ST_MakePoint(26.851, 58.028), 4326), 'Kärbseseened'),
   (ST_SetSRID(ST_MakePoint(25.013, 58.383), 4326), 'Pilvikud');
